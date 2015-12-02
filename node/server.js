18b05631203b7be3cd8d0e6154a415d27a3234ca
John Mart Belamide <belamide09@gmail.com>
2015-12-02 17:02:07 +0800

    fix bug

--- node/server.js
+++ node/server.js
@@ -122,6 +122,7 @@ io.on('connection',function(socket) {
 
 	socket.on('send_message',function(data) {
 
+		messages.push(data);
 		if ( messages.length == 100 ) {
 			messages = messages.slice(1);
 		}
