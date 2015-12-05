017f562c6017b85bf597fe3789cde3d2e08c016a
John Mart Belamide <belamide09@gmail.com>
2015-12-05 16:18:13 +0800

    fix case sensitive bugs

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,7 +24,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'mainroom', 'action' => 'index'));
+	Router::connect('/', array('controller' => 'MainRoom', 'action' => 'index'));
 	Router::connect('/ChatRoom/:id',array('controller' => 'ChatRoom', 'action' => 'index'));
 	Router::connect('/VideoCall',array('controller' => 'VideoCall', 'action' => 'index'));
 
